e816fccd9e3429195974d2d527d3b84d73cda6d0
kenny <kenny909@mat.umk.pl>
2024-07-19 14:56:31 +0200

    simple structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <main>
+      <form>
+        <section className='inputSection'>
+          <h3>Credentials</h3>
+          <InputItem name='First Name'></InputItem>
+          <InputItem name='Last Name'></InputItem>
+          <InputItem name='Address'></InputItem>
+          <InputItem name='Phone number'></InputItem>
+        </section>
+        <section className='inputSection'>
+          <h3>Educational experience</h3>
+          <InputItem name='School name'></InputItem>
+          <InputItem name='Title of study'></InputItem>
+          <InputItem name='start of study' type='date'></InputItem>
+          <InputItem name='end of study' type='date'></InputItem>
+        </section>
+        <section className='inputSection'>
+          <h3>Practical experience</h3>
+          <InputItem name='Company name'></InputItem>
+          <InputItem name='position title'></InputItem>
+          <InputItem name='main responsibilities'></InputItem>
+          <InputItem name='start of work' type='date'></InputItem>
+          <InputItem name='end of work' type='date'></InputItem>
+        </section>
+
+      </form>
+
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +51,21 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </p> */}
+    </main>
   )
+}
+
+function InputItem({ name, type = 'text' }) {
+  if (!name) {
+    return;
+  }
+  return (
+    <label>{name + ': '}
+      <input type={type} />
+    </label>
+  );
+
 }
 
 export default App
